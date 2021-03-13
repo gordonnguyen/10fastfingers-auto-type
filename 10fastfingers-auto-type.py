ecd50f1ea9d1798a7e0c0fbc01995682e61dc482
Gordon Nguyen <maixanh45@yahoo.com>
2021-03-13 12:08:42 -0600

    Optimized code

--- 10fastfingers-auto-type.py
+++ 10fastfingers-auto-type.py
@@ -20,10 +20,11 @@ SPACE_KEY = "\ue00d"
 # OTHERS
 LINE = '<>'*20
 WORDS_PER_LINE = 12
-
+RESULT_VIEWING_TIME = 15
+FINAL_VIEWING_TIME = 60*60  # 1 hour
 
 def main():
-    # Initilize settings from .ini
+    # Initilize settings
     setting = read_ini_settings()
     typing_test_url = setting['typing_test_url']
     use_account = setting['use_account']
@@ -50,18 +51,15 @@ def main():
             target_word = driver.find_element_by_css_selector(TARGET_WORD_SLTCR).text
             if target_word != '':
                 type_word(target_word, driver)
-
                 if extract_text_to_file == True:
                     word_count += 1
-                    if word_count % WORDS_PER_LINE == 0:
-                        extracted_text += target_word + '\n'
-                    else:
-                        extracted_text += target_word+' '
-
+                    extracted_text = accumulate_words(target_word, word_count, extracted_text)
                 time.sleep(word_per_sec)
             else:
+                time.sleep(RESULT_VIEWING_TIME)
                 break
 
+    # extract words from test to file
     if extract_text_to_file:
         with open('competition_text_file.txt', 'w') as text_f:
             text_f.write(extracted_text)
@@ -69,19 +67,41 @@ def main():
     print('\n'+LINE+"\nALL DONE! Check your browser for results!")
 
     # Retain browser for viewing
-    time.sleep(60*60)
+    time.sleep(FINAL_VIEWING_TIME)
 
-def type_word(target_word, driver):
-    driver.find_element_by_id(INPUT_FIELD_ID).send_keys(target_word+SPACE_KEY)
+def read_ini_settings():
+    setting_temp = []
+    setting = {}
+    num_of_value = 1
+    seperate_symbol = '=' 
+
+    # Open and read setting.ini
+    with open('setting.ini') as f:
+        for item in f:
+            if not (item.startswith('#') or item.startswith('\n')):
+                setting_temp.append(item.split(seperate_symbol))  # Will result with a 2D list
+
+    # Convert setting list to dictionary
+        for x in range(len(setting_temp)):
+            for y in range(num_of_value):
+                key = setting_temp[x][y].strip()
+                value = setting_temp[x][y+1].strip()
+                if value.lower() == 'true':
+                    value = True
+                setting[key] = value
+    return setting
 
 def sign_in(driver, setting):
     email = setting['email']
     password = setting['password']
 
+    # Fill text fields with account info
     driver.get(SIGNIN_URL)
     WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.ID,USERNAME_ID)))
     driver.find_element_by_id(USERNAME_ID).send_keys(email)
     driver.find_element_by_id(PASSWORD_ID).send_keys(password)
+    
+    # Try logging in
     driver.find_element_by_id(LOGIN_BTN_ID).click()
     try:
         WebDriverWait(driver, 15).until(EC.url_changes(SIGNIN_URL))
@@ -90,24 +110,14 @@ def sign_in(driver, setting):
     else:
         print('Sign in successfully!')
 
-def read_ini_settings():
-    setting_temp = []
-    setting = {}
-    pair_num = 2
-    file_key_value_sep = '=' 
+def type_word(target_word, driver):
+    driver.find_element_by_id(INPUT_FIELD_ID).send_keys(target_word+SPACE_KEY)
 
-    with open('setting.ini') as f:
-        for item in f:
-            if not (item.startswith('#') or item.startswith('\n')):
-                setting_temp.append(item.split(file_key_value_sep))  # Will result with a 2D list
+def accumulate_words(target_word, word_count, extracted_text):
+    if word_count % WORDS_PER_LINE == 0:
+        extracted_text += target_word + '\n'
+    else:
+        extracted_text += target_word+' '
+    return extracted_text
 
-        for x in range(len(setting_temp)):
-            for y in range(pair_num-1):
-                key = setting_temp[x][y].strip()
-                value = setting_temp[x][y+1].strip()
-                if value.lower() == 'true':
-                    value = True
-                setting[key] = value
-    return setting
-        
 main()
